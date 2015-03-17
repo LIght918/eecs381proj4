1d0f0c58cb5be22ddccb53313ab4fcfb1523423b
ZucchiniSky <scottbommarito@gmail.com>
2015-03-17 16:15:53 -0400

    tidying up

--- Island.cpp
+++ Island.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 // initialize then output constructor message
-Island::Island(const string &name_, Point position_, double fuel_ = 0., double production_rate_ = 0.) :
+Island::Island(const string &name_, Point position_, double fuel_, double production_rate_) :
         Sim_object(name_), position(position_), fuel(fuel_), production_rate(production_rate_)
 {
     cout << "Island " << get_name() << " constructed" << endl;
@@ -36,20 +36,20 @@ void Island::accept_fuel(double amount)
 }
 
 // if production_rate > 0, compute production_rate * unit time, and add to amount, and print an update message
-void Island::update() override
+void Island::update()
 {
     fuel += production_rate;
     cout << "Island " << get_name() << " now has " << fuel << " tons" << endl;
 }
 
 // output information about the current state
-void Island::describe() const override
+void Island::describe() const
 {
     cout << "\nIsland " << get_name() << " at position " << position << "\nFuel available: " << fuel << " tons";
 }
 
 // ask model to notify views of current state
-void Island::broadcast_current_state() override
+void Island::broadcast_current_state()
 {
     g_Model_ptr->notify_location(get_name(), position);
 }
