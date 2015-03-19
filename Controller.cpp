25511f7b62eb3dc71eab70235c985d8cd1c2d1d1
ZucchiniSky <scottbommarito@gmail.com>
2015-03-19 15:35:45 -0400

    remove extra stuff

--- Controller.cpp
+++ Controller.cpp
@@ -184,13 +184,11 @@ void Controller::ship_course(Ship *ship)
 }
 void Controller::ship_position(Ship *ship)
 {
-    Point destination(read_double(), read_double());
-    ship->set_destination_position_and_speed(destination, read_speed());
+    ship->set_destination_position_and_speed(Point(read_double(), read_double()), read_speed());
 }
 void Controller::ship_destination(Ship *ship)
 {
-    Island *destination = read_island();
-    ship->set_destination_position_and_speed(destination->get_location(), read_speed());
+    ship->set_destination_position_and_speed(read_island()->get_location(), read_speed());
 }
 void Controller::ship_load_at(Ship *ship)
 {
