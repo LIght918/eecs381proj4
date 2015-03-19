e4df960f35618177e9ddc43001fa0f1bc93840fb
ZucchiniSky <scottbommarito@gmail.com>
2015-03-19 18:34:21 -0400

    size

--- View.cpp
+++ View.cpp
@@ -53,7 +53,7 @@ void View::draw()
 	vector<string> objects_out_of_map;
 	// make sure the map is big enough for what we want
 	map_view.reserve(size);
-	for_each(map_view.begin(), map_view.end(), [](vector<string>& row) {row.reserve(size);});
+	for_each(map_view.begin(), map_view.end(), [size](vector<string>& row) {row.reserve(size);});
 	// fill the map with the empty object icon
 	for_each(map_view.begin(), map_view.end(), [](vector<string>& row) {fill(row.begin(), row.end(), VIEW_NO_OBJECT);});
 
